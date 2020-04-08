a03307343f72a97103ea5e349f7f03423f8e01b8
Axel <axel.diner@gmail.com>
2020-04-09 00:09:37 +0200

    API RCSA

--- curl.php
+++ curl.php
@@ -1,11 +1,18 @@
 <?php
-$curl = curl_init('https://entreprise.data.gouv.fr/api/rncs/v1/fiches_identite/827905522');
+
+$APIRCSA = 'https://entreprise.data.gouv.fr/api/rncs/v1/fiches_identite/827905522'.RCS;
+$curl = curl_init($APIRCSA);
 $data = curl_exec($curl);
 if ($data===false) {
 var_dump(curl_error($curl));
 }
 else {
 $data = json.decode($data, true);
+define("NAMESOC", $data[dossier_entreprise_greffe_principal][etablissements][1][nom_commercial]);
+efine("CAPITAL", $data[dossier_entreprise_greffe_principal][etablissement_principal[capital]);
+define("ADRESSSOC", $data[dossier_entreprise_greffe_principal][etablissements][0][adresse_ligne_1].$data[dossier_entreprise_greffe_principal][etablissements][0][adresse_code_postal].$data[dossier_entreprise_greffe_principal][etablissements][0][adresse_ville]);
+define("VILLESOC", $data[dossier_entreprise_greffe_principal][code_greffe]);
+
 echo '<pre>';
 var_dump($data[dossier_entreprise_greffe_principal][code_greffe]);
 echo '<pre>';
